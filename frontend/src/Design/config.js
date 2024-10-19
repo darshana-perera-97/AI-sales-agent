90f4c79b41f8888e18f01e8c37ade7f4b4975339
Darshana Perera <ds.perera.1997@gmail.com>
2024-10-19 10:30:58 +0530

    update to localhost

--- frontend/src/Design/config.js
+++ frontend/src/Design/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_BASE_URL: "http://108.181.199.62:5000",
-  // API_BASE_URL: "http://localhost:5000",
+  // API_BASE_URL: "http://108.181.199.62:5000",
+  API_BASE_URL: "http://localhost:5000",
   // API_BASE_IP: "http://localhost",
   // API_BASE_IP: "http://108.181.199.62",
 };
