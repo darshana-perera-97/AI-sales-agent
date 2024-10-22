ab73314868403ff75adffdb52e0ea77cb05fdc43
Darshana Perera <91814341+darshana-perera-97@users.noreply.github.com>
2024-10-22 11:53:27 +0530

    Update config.js

---
+++ frontend/src/Design/config.js
@@ -0,0 +1,7 @@
+const config = {
+  API_BASE_URL: "http://localhost:5000", // Update here for production
+  // API_BASE_URL: "http://108.181.199.62:5000", // Update here for production
+  
+};
+
+export default config;
